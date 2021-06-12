81d2cd818f83548171ecc96e8dec62902ccd9490
zaur <zaur.nadiraliev.228@gmail.com>
2021-06-13 00:13:16 +0300

    Added TupleDecoderTest

---
+++ test/TupleDecoderTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Klimick\Decode\Test;
+
+use Eris\Generator\AssociativeArrayGenerator;
+use Klimick\Decode\Test\Helper\Check;
+use Klimick\Decode\Test\Helper\DecoderGenerator;
+use PHPUnit\Framework\TestCase;
+use function Klimick\Decode\Decoder\tuple;
+use function Klimick\Decode\Test\Helper\forAll;
+
+final class TupleDecoderTest extends TestCase
+{
+    public function testValid(): void
+    {
+        [$firstPropDecoder, $firstPropGenerator] = DecoderGenerator::generate();
+        [$secondPropDecoder, $secondPropGenerator] = DecoderGenerator::generate();
+
+        $tupleDecoder = tuple($firstPropDecoder, $secondPropDecoder);
+
+        $tupleGenerator = new AssociativeArrayGenerator([
+            $firstPropGenerator,
+            $secondPropGenerator,
+        ]);
+
+        forAll($tupleGenerator)
+            ->withMaxSize(50)
+            ->then(Check::thatValidFor($tupleDecoder));
+    }
+}
