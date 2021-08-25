7a90aa45a3b85cd2e261572efdce418f53398130
andrew <klimichkartorgnusov@gmail.com>
2021-08-25 11:47:52 +0300

    SeePsalmIssues scoped to code block

--- psalm-test/Integration/Assertion/Collector/HaveCodeAssertionCollector.php
+++ psalm-test/Integration/Assertion/Collector/HaveCodeAssertionCollector.php
@@ -6,6 +6,8 @@ namespace Klimick\PsalmTest\Integration\Assertion\Collector;
 
 use Fp\Functional\Option\Option;
 use Klimick\PsalmTest\Integration\Assertion\Assertions;
+use PhpParser\Node;
+use Psalm\CodeLocation;
 use Psalm\Type;
 use Psalm\Type\Atomic\TClosure;
 use function Fp\Collection\first;
@@ -14,13 +16,36 @@ final class HaveCodeAssertionCollector implements AssertionCollectorInterface
 {
     public static function collect(Assertions $data, AssertionCollectingContext $context): Option
     {
+        return Option::do(function() use ($data, $context) {
+            $closure_code_location = yield self::getClosureCodeLocation($context);
+            $closure_return_type = yield self::getClosureReturnType($context);
+
+            return $data->with(
+                new HaveCodeAssertionData($closure_code_location, $closure_return_type)
+            );
+        });
+    }
+
+    /**
+     * @return Option<Type\Union>
+     */
+    private static function getClosureReturnType(AssertionCollectingContext $context): Option
+    {
         return first($context->assertion_call->args)
             ->flatMap(fn($arg) => $context->getSingleAtomicType($arg->value))
             ->filter(fn($atomic) => $atomic instanceof TClosure)
-            ->map(fn($atomic) => $atomic->return_type ?? Type::getVoid())
-            ->map(fn($actual_return_type) => $data->with(
-                new HaveCodeAssertionData($context->getCodeLocation(), $actual_return_type)
-            ));
+            ->map(fn($atomic) => $atomic->return_type ?? Type::getVoid());
+    }
+
+    /**
+     * @return Option<CodeLocation>
+     */
+    private static function getClosureCodeLocation(AssertionCollectingContext $context): Option
+    {
+        return first($context->assertion_call->args)
+            ->map(fn($arg) => $arg->value)
+            ->filter(fn($arg_value) => $arg_value instanceof Node\Expr\Closure)
+            ->map(fn($closure_arg) => new CodeLocation($context->event->getStatementsSource(), $closure_arg));
     }
 
     public static function isSupported(AssertionCollectingContext $context): bool
--- psalm-test/Integration/Assertion/Reconciler/SeePsalmIssuesAssertionReconciler.php
+++ psalm-test/Integration/Assertion/Reconciler/SeePsalmIssuesAssertionReconciler.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Klimick\PsalmTest\Integration\Assertion\Reconciler;
 
+use Closure;
 use Fp\Functional\Option\Option;
 use Klimick\PsalmTest\Integration\Assertion\Assertions;
+use Klimick\PsalmTest\Integration\Assertion\Collector\HaveCodeAssertionData;
 use Klimick\PsalmTest\Integration\Assertion\Collector\SeePsalmIssue;
 use Klimick\PsalmTest\Integration\Assertion\Collector\SeePsalmIssuesData;
 use Klimick\PsalmTest\Integration\Assertion\Issue\SeePsalmIssueAssertionFailed;
@@ -20,7 +22,9 @@ final class SeePsalmIssuesAssertionReconciler implements AssertionReconcilerInte
     {
         return Option::do(function() use ($data) {
             $seePsalmIssues = yield $data(SeePsalmIssuesData::class);
-            $unhandledIssues = self::handle($seePsalmIssues);
+            $haveCodeAssertion = yield $data(HaveCodeAssertionData::class);
+
+            $unhandledIssues = self::handle($seePsalmIssues, $haveCodeAssertion);
 
             return yield !empty($unhandledIssues)
                 ? Option::some(new SeePsalmIssueAssertionFailed($unhandledIssues, $seePsalmIssues->code_location))
@@ -31,43 +35,50 @@ final class SeePsalmIssuesAssertionReconciler implements AssertionReconcilerInte
     /**
      * @return list<SeePsalmIssue>
      */
-    private static function handle(SeePsalmIssuesData $seePsalmIssues): array
+    private static function handle(SeePsalmIssuesData $seePsalmIssues, HaveCodeAssertionData $haveCodeAssertion): array
     {
         $issuesData = IssueBuffer::getIssuesData();
 
-        // Treat all issues as unhandled if no actual issues for analysed file
         if (!array_key_exists($seePsalmIssues->code_location->file_path, $issuesData)) {
             return $seePsalmIssues->issues;
         }
 
-        // All expected issues
-        $expectedIssues = reindex(
-            $seePsalmIssues->issues,
-            fn(SeePsalmIssue $i) => "[{$i->type}]:[{$i->message}]",
-        );
+        $expectedIssues = reindex($seePsalmIssues->issues, self::toKeyFn());
+        $actualIssues = reindex($issuesData[$seePsalmIssues->code_location->file_path], self::toKeyFn());
+        $handledIssues = filter($actualIssues, self::isHandled($haveCodeAssertion, $expectedIssues), preserveKeys: true);
 
-        // All actual issues from Psalm buffer
-        $actualIssues = reindex(
-            $issuesData[$seePsalmIssues->code_location->file_path],
-            fn(IssueData $i) => "[{$i->type}]:[{$i->message}]",
-        );
-
-        // Catch all expected issues from buffer
-        $handledIssues = filter(
-            $actualIssues,
-            fn(IssueData $i) => array_key_exists("[{$i->type}]:[{$i->message}]", $expectedIssues),
-            preserveKeys: true
-        );
-
-        // Remove found expected issues from Psalm buffer
         foreach ($handledIssues as $i) {
             IssueBuffer::remove($i->file_path, $i->type, $i->from);
         }
 
-        // Rest expected issues treat as unhandled
-        return filter(
-            $expectedIssues,
-            fn(SeePsalmIssue $i) => !array_key_exists("[{$i->type}]:[{$i->message}]", $handledIssues),
-        );
+        return filter($expectedIssues, fn(SeePsalmIssue $i) => !array_key_exists(self::toKey($i), $handledIssues));
+    }
+
+    /**
+     * @return Closure(object{type: string, message: string}): string
+     */
+    private static function toKeyFn(): Closure
+    {
+        return fn(object $i) => self::toKey($i);
+    }
+
+    /**
+     * @param object{type: string, message: string} $i
+     * @return non-empty-string
+     */
+    private static function toKey(object $i): string
+    {
+        return "[{$i->type}]:[{$i->message}]";
+    }
+
+    /**
+     * @param array<string, SeePsalmIssue> $expectedIssues
+     * @return Closure(IssueData): bool
+     */
+    private static function isHandled(HaveCodeAssertionData $haveCodeAssertion, array $expectedIssues): Closure
+    {
+        return fn(IssueData $i) => array_key_exists(self::toKey($i), $expectedIssues) &&
+            $i->from >= $haveCodeAssertion->code_location->raw_file_start &&
+            $i->to <= $haveCodeAssertion->code_location->raw_file_end;
     }
 }
