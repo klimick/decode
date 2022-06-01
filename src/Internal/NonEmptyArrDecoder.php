36e066f9af1517bc8246aa8b46d77739560f759a
adrew <klimichkartorgnusov@gmail.com>
2022-06-01 20:20:33 +0300

    Remove unuseful type assertion

--- src/Internal/NonEmptyArrDecoder.php
+++ src/Internal/NonEmptyArrDecoder.php
@@ -6,8 +6,6 @@ namespace Klimick\Decode\Internal;
 
 use Fp\Functional\Either\Either;
 use Klimick\Decode\Context;
-use Klimick\Decode\Decoder\Valid;
-use Klimick\Decode\Decoder\Invalid;
 use Klimick\Decode\Decoder\AbstractDecoder;
 use Klimick\Decode\Decoder\DecoderInterface;
 use function Klimick\Decode\Decoder\arrayOf;
@@ -36,13 +34,8 @@ final class NonEmptyArrDecoder extends AbstractDecoder
         return "non-empty-array<{$this->keyDecoder->name()}, {$this->valDecoder->name()}>";
     }
 
-
     public function decode(mixed $value, Context $context): Either
     {
-        // todo: generics are compared nominally??
-        //  Either<Invalid, Valid<non-empty-array<TKey:ArrDecoder as array-key, TVal:ArrDecoder as mixed>>>
-        //  Either<Invalid, Valid<non-empty-array<TKey:NonEmptyArrDecoder as array-key, TVal:NonEmptyArrDecoder as mixed>>>
-        /** @var Either<Invalid, Valid<non-empty-array<TKey, TVal>>> */
         return arrayOf($this->keyDecoder, $this->valDecoder)
             ->decode($value, $context)
             ->flatMap(fn($valid) => 0 !== count($valid->value)
