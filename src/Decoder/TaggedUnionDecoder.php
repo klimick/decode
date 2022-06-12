aed5e642ce8e5f4171b64e86043c06b789c59f46
adrew <klimichkartorgnusov@gmail.com>
2022-06-12 10:48:44 +0300

    More detailed error for TaggedUnionDecoder

--- src/Decoder/TaggedUnionDecoder.php
+++ src/Decoder/TaggedUnionDecoder.php
@@ -7,6 +7,8 @@ namespace Klimick\Decode\Decoder;
 use Fp\Functional\Either\Either;
 use Fp\Functional\Option\Option;
 use Klimick\Decode\Context;
+use Klimick\Decode\Decoder\Error\DecodeErrorInterface;
+use Klimick\Decode\Decoder\Error\UndefinedError;
 use function Fp\Collection\at;
 use function Fp\Collection\map;
 use function Fp\Evidence\proveString;
@@ -37,19 +39,21 @@ final class TaggedUnionDecoder extends AbstractDecoder
 
     public function decode(mixed $value, Context $context): Either
     {
-        return $this->getTaggedDecoderFor($value)
-            ->map(fn($decoder) => $decoder->decode($value, $context))
-            ->getOrCall(fn() => invalid($context));
+        return $this->getTaggedDecoderFor($context, $value)
+            ->flatMap(fn($decoder) => $decoder->decode($value, $context));
     }
 
     /**
-     * @return Option<DecoderInterface<T>>
+     * @return Either<non-empty-list<DecodeErrorInterface>, DecoderInterface<T>>
      */
-    private function getTaggedDecoderFor(mixed $value): Option
+    private function getTaggedDecoderFor(Context $context, mixed $value): Either
     {
         return Option::fromNullable(is_array($value) ? $value : null)
             ->flatMap(fn($array) => at($array, $this->tag))
             ->flatMap(fn($tag) => proveString($tag))
-            ->flatMap(fn($tag) => at($this->decoders, $tag));
+            ->flatMap(fn($tag) => at($this->decoders, $tag))
+            ->toRight(fn() => [
+                new UndefinedError($context($this, actual: null, key: $this->tag)),
+            ]);
     }
 }
