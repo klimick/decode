43ad18891c2803f68ea1be60ce3ea87d3f691442
adrew <klimichkartorgnusov@gmail.com>
2022-06-12 10:48:31 +0300

    More detailed typename for ConstantlyDecoder

--- src/Decoder/ConstantlyDecoder.php
+++ src/Decoder/ConstantlyDecoder.php
@@ -6,6 +6,14 @@ namespace Klimick\Decode\Decoder;
 
 use Fp\Functional\Either\Either;
 use Klimick\Decode\Context;
+use function Fp\Collection\every;
+use function Fp\Collection\keys;
+use function Fp\Collection\map;
+use function get_class;
+use function get_debug_type;
+use function implode;
+use function is_int;
+use function is_object;
 
 /**
  * @template T
@@ -21,7 +29,39 @@ final class ConstantlyDecoder extends AbstractDecoder
 
     public function name(): string
     {
-        return 'constant';
+        return 'constant<' . self::getTypename($this->constant) . '>';
+    }
+
+    /**
+     * @return non-empty-string
+     * @psalm-pure
+     */
+    private static function getTypename(mixed $value): string
+    {
+        /** @var non-empty-string */
+        return match (get_debug_type($value)) {
+            'null' => 'null',
+            'bool' => $value ? 'true' : 'false',
+            'int', 'float' => (string) $value,
+            'string' => "'{$value}'",
+            'array' => self::getArrayTypeName($value),
+            default => is_object($value) ? get_class($value) : 'unknown',
+        };
+    }
+
+    /**
+     * @return non-empty-string
+     * @psalm-pure
+     */
+    private static function getArrayTypeName(array $arr): string
+    {
+        $isList = every(keys($arr), fn($k) => is_int($k));
+
+        $types = $isList
+            ? map($arr, fn(mixed $v) => self::getTypename($v))
+            : map($arr, fn(mixed $v, string|int $k) => $k . ': ' . self::getTypename($v));
+
+        return 'array{' . implode(', ', $types) . '}';
     }
 
     public function decode(mixed $value, Context $context): Either
