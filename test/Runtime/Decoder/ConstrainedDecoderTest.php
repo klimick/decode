042b85f015417db0e8ae389d44e66d56e2bf0584
andrew <klimichkartorgnusov@gmail.com>
2022-06-14 18:18:27 +0300

    Add ConstrainedDecoderTest

---
+++ test/Runtime/Decoder/ConstrainedDecoderTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Klimick\Decode\Test\Runtime\Decoder;
+
+use Klimick\Decode\Report\ConstraintErrorReport;
+use Klimick\Decode\Report\ErrorReport;
+use Klimick\Decode\Test\Runtime\Assert;
+use PHPUnit\Framework\TestCase;
+use function Klimick\Decode\Constraint\minLength;
+use function Klimick\Decode\Decoder\decode;
+use function Klimick\Decode\Decoder\null;
+use function Klimick\Decode\Decoder\string;
+use function Klimick\Decode\Decoder\union;
+
+final class ConstrainedDecoderTest extends TestCase
+{
+    public function testTypename(): void
+    {
+        Assert::name('string', string()->constrained(minLength(is: 3)));
+    }
+
+    public function testDecodeFailedIfConstraintFailed(): void
+    {
+        $constraint = minLength(is: 3);
+        $decoder = string()->constrained($constraint);
+
+        Assert::decodeFailed(
+            expected: new ErrorReport([
+                new ConstraintErrorReport('$', 'vi', $constraint->metadata())
+            ]),
+            actual: decode('vi', $decoder),
+        );
+    }
+
+    public function testDecodeSuccessIfConstraintSuccess(): void
+    {
+        $constraint = minLength(is: 2);
+        $decoder = string()->constrained($constraint);
+
+        Assert::decodeSuccess(
+            expectedValue: 'vi',
+            actualDecoded: decode('vi', $decoder),
+        );
+    }
+
+    public function testConstraintIgnoreNullValues(): void
+    {
+        $constraint = minLength(is: 2);
+        $decoder = union(string(), null())->constrained($constraint);
+
+        Assert::decodeSuccess(
+            expectedValue: null,
+            actualDecoded: decode(null, $decoder),
+        );
+    }
+}
