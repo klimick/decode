9087295871965209f267e3f078d0449f6698f42b
adrew <klimichkartorgnusov@gmail.com>
2022-06-14 19:56:08 +0300

    Add ArrayKeyDecoderTest

---
+++ test/Runtime/Decoder/ArrayKeyDecoderTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Klimick\Decode\Test\Runtime\Decoder;
+
+use Klimick\Decode\Report\ErrorReport;
+use Klimick\Decode\Report\TypeErrorReport;
+use Klimick\Decode\Test\Runtime\Assert;
+use PHPUnit\Framework\TestCase;
+use function Klimick\Decode\Decoder\arrayKey;
+use function Klimick\Decode\Decoder\decode;
+
+final class ArrayKeyDecoderTest extends TestCase
+{
+    public function testTypename(): void
+    {
+        Assert::name('array-key', arrayKey());
+    }
+
+    public function testDecodeFailedWithNonArrayKeyValue(): void
+    {
+        $decoder = arrayKey();
+
+        Assert::decodeFailed(
+            expected: new ErrorReport([
+                new TypeErrorReport('$', $decoder->name(), 1.42),
+            ]),
+            actual: decode(1.42, $decoder),
+        );
+    }
+
+    public function testDecodeSuccessForIntArrayKey(): void
+    {
+        Assert::decodeSuccess(
+            expectedValue: 42,
+            actualDecoded: decode(42, arrayKey()),
+        );
+    }
+
+    public function testDecodeSuccessForStringArrayKey(): void
+    {
+        Assert::decodeSuccess(
+            expectedValue: 'key-42',
+            actualDecoded: decode('key-42', arrayKey()),
+        );
+    }
+}
